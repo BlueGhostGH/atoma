8f6519df092928d3f2822196abb5b83692203780
George Dragomir <george.dragomir.x@gmail.com>
2026-01-16 00:55:40 +0200

    Set up scratchpad for package experimentation

---
+++ scratchpad/index.ts
@@ -0,0 +1,16 @@
+import { Trait } from "atoma";
+
+// Example: Define a Show trait
+interface ShowSignature {
+  show: (self: unknown) => string;
+}
+
+const Show = Trait.makeTrait<"Show", ShowSignature>("Show");
+
+// Example: Implement Show for a custom type
+Trait.registerImpl(Show, "Number", {
+  show: (self) => `Number(${self})`,
+});
+
+// Example: Use the trait
+console.log(Trait.invoke(Show, "Number", "show", 42));
